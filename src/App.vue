c3c4b502b297a79ba2c0254830300bde11b6b010
vertocode <evertonvanoni1@gmail.com>
2023-08-07 21:48:25 -0300

    feat: add some animations to navbar
    (cherry picked from commit 11bd5d80c730fe06c22d7d2562d6ab15edac92cb)

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Navbar />
+
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<script setup lang="ts">
+
+import Navbar from "@/components/Navbar.vue";
+</script>
+
+<style scoped>
+</style>
